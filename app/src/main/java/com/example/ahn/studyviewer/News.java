7ad4a24e321eadbd7fd6ea02443760a312f08d06
KimHyunse <anzmf12@naver.com>
2017-02-22 20:31:04 +0900

    * News VO 추가

---
+++ app/src/main/java/com/example/ahn/studyviewer/News.java
@@ -0,0 +1,36 @@
+package com.example.ahn.studyviewer;
+
+/**
+ * Created by 고물 on 2017-02-22.
+ */
+
+public class News {
+
+    private String title;
+    private String content;
+    private String regDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+}
